3700b81dd22c65b13d9310a4315087f91bd551bf
riley.lin <rileylin.space@gmail.com>
2023-09-21 17:56:38 +0800

    add products

--- assets/js/index.js
+++ assets/js/index.js
@@ -45,3 +45,19 @@ const showElemOnScroll = function () {
 }
 
 addEventOnElem(window, "scroll", showElemOnScroll)
+
+// product filter
+const filterBtns = document.querySelectorAll("[data-filter-btn]")
+const filterBox = document.querySelector("[data-filter]")
+
+let lastClickedFilterBtn = filterBtns[0]
+
+const filter = function () {
+    lastClickedFilterBtn.classList.remove("active")
+    this.classList.add("active")
+    lastClickedFilterBtn = this
+
+    filterBox.setAttribute("data-filter", this.dataset.filterBtn)
+}
+
+addEventOnElem(filterBtns, "click", filter)
