47032d2487784708b34736c6aed5442296f7d797
riley.lin <rileylin.space@gmail.com>
2023-09-21 19:05:03 +0800

    add back to top button

--- assets/js/index.js
+++ assets/js/index.js
@@ -33,16 +33,21 @@ const closeNavbar = function () {
 
 addEventOnElem(navbarLinks, "click", closeNavbar)
 
-// header active when window scroll down to 100px
+// header & back top btn active when window scroll down to 100px
 const header = document.querySelector("[data-header]")
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 const showElemOnScroll = function () {
     if (window.scrollY > 100) {
-        header.classList.add("active")
+        header.classList.add("active");
+        backTopBtn.classList.add("active");
     } else {
-        header.classList.remove("active")
+        header.classList.remove("active");
+        backTopBtn.classList.remove("active");
     }
 }
+
+addEventOnElem(window, "scroll", showElemOnScroll);
 
 addEventOnElem(window, "scroll", showElemOnScroll)
 
